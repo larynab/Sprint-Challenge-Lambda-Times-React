24267f5f516dfcf5640c70788764c6d70f7f7fd2
larynab <fsovreign@gmail.com>
2019-02-22 08:59:34 -0800

    tabs-beta

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -8,7 +8,7 @@ const Cards = props => {
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
       {props.cards.map(
-        card => <Card key={card.headline} card={card} />)}    
+        card => <Card key={card.headline} headline={card.headline} img={card.img} author={card.author} />)}    
     </div>
   )
 }
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Tab = props => {
+const Tab = tab => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
@@ -12,7 +12,7 @@ const Tab = props => {
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
     >
-      {props.tab.toUpperCase()}
+      {tab.toUpperCase()}
     </div>
   );
 };
